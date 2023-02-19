5b32969787c750907d3313c78b427966073fa774
baegteun <baegteun@gmail.com>
2023-02-19 15:55:59 +0900

    :tada: :: Begin A Project

---
+++ WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by 최형우 on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
