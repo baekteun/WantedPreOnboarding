dbf909e45e49de8b24c4ad86496672081cccf702
baegteun <baegteun@gmail.com>
2023-02-19 16:09:07 +0900

    :sparkles: :: BaseViewController Lifecycle Publisher

--- WantedPreOnboarding/Feature/BaseFeature/Sources/BaseFeature/BaseViewController.swift
+++ WantedPreOnboarding/Feature/BaseFeature/Sources/BaseFeature/BaseViewController.swift
@@ -4,16 +4,42 @@ import UIKit
 open class BaseViewController: UIViewController, BoundsProvidable {
 
     // MARK: - Properties
+    private let viewDidLoadSubject = PassthroughSubject<Void, Never>()
+    private let viewWillAppearSubject = PassthroughSubject<Void, Never>()
+    private let viewDidAppearSubject = PassthroughSubject<Void, Never>()
+    private let viewWillDisappearSubject = PassthroughSubject<Void, Never>()
+    private let viewDidDisappearSubject = PassthroughSubject<Void, Never>()
     public var bag = Set<AnyCancellable>()
 
     // MARK: - Lifecycle
 
     open override func viewDidLoad() {
         super.viewDidLoad()
+        viewDidLoadSubject.send(())
         addView()
         setLayout()
         configureViewController()
         bind()
+    }
+
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewWillAppearSubject.send(())
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewDidAppearSubject.send(())
+    }
+
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewWillDisappearSubject.send(())
+    }
+
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        viewDidDisappearSubject.send(())
     }
 
     // MARK: - Override
@@ -27,4 +53,26 @@ open class BaseViewController: UIViewController, BoundsProvidable {
     }
 
     open func bind() { }
+}
+
+extension BaseViewController: LifecyclePublishable {
+    public var viewDidLoadPublisher: AnyPublisher<Void, Never> {
+        self.viewDidLoadSubject.eraseToAnyPublisher()
+    }
+    
+    public var viewWillAppearPublisher: AnyPublisher<Void, Never> {
+        self.viewWillAppearSubject.eraseToAnyPublisher()
+    }
+    
+    public var viewDidAppearPublisher: AnyPublisher<Void, Never> {
+        self.viewDidAppearSubject.eraseToAnyPublisher()
+    }
+    
+    public var viewWillDisappearPublisher: AnyPublisher<Void, Never> {
+        self.viewWillDisappearSubject.eraseToAnyPublisher()
+    }
+    
+    public var viewDidDisappearPublisher: AnyPublisher<Void, Never> {
+        self.viewDidDisappearSubject.eraseToAnyPublisher()
+    }
 }
